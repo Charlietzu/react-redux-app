a41023f32e579b6568698efe756b9c674102728a
Caio César Silva <caiocesarsilva08@gmail.com>
2020-04-09 20:24:51 -0300

    Dev Enviroment
    Setup of the development enviroment.

---
+++ webpack.config.dev.js
@@ -0,0 +1,58 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+//node enviroment declaration (development mode)
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  //run in development mode
+  mode: "development",
+  target: "web",
+  //source maps let us see our original code when debugging in the browser
+  devtool: "cheap-module-source-map",
+  //entry point of the application
+  entry: "./src/index",
+  /*where we want to webpack to output 
+  note: webpack doesn't output code in development mode. it serves our app from memory*/
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  /*setting our server to development */
+  devServer: {
+    //minimal information in the terminal
+    stats: "minimal",
+    //overlay any errors that occur in the browser
+    overlay: true,
+    //all request will be sent to index.html
+    historyApiFallback: true,
+    //we need these 3 lines bellow because of a issue ocurring when using the latest version of Google Chrome
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        //how to find our JavaScript files (js or jsx files)
+        test: /\.(js|jsx)$/,
+        //ignore node modules
+        exclude: /node_modules/,
+        //tell webpack what to do with these JavaScript files
+        use: ["babel-loader", "eslint-loader"],
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};

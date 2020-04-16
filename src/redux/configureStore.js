952bdc2c85480ef258661bd8f3f1c87ebf4fd7d8
Caio César Silva <caiocesarsilva08@gmail.com>
2020-04-16 16:00:55 -0300

    Production build
    I configured the production build setup files.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,19 +1,6 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./reducers/index";
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
-import thunk from "redux-thunk";
-
-export default function configureStore(initialState) {
-  /**add support for Redux dev tools */
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-  return createStore(
-    rootReducer,
-    initialState,
-    /**reduxImmutableStateInvariant will warn us if we accidentally mutate
-     * redux state
-     */
-    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
-  );
+//Use CommonJS require below so we can dynamically import during build-time.
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./configureStore.prod");
+} else {
+  module.exports = require("./configureStore.dev");
 }

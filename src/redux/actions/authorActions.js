6513a1474913355fbd1fb6a1e6095b260444d1b2
Caio César Silva <caiocesarsilva08@gmail.com>
2020-04-18 14:19:12 -0300

    Author Api and Redux
    Implementation of authorApi's missing methods and it's functions in Redux.

--- src/redux/actions/authorActions.js
+++ src/redux/actions/authorActions.js
@@ -6,6 +6,21 @@ export function loadAuthorsSuccess(authors) {
   return { type: types.LOAD_AUTHORS_SUCCESS, authors };
 }
 
+export function createAuthorSuccess(author) {
+  return {
+    type: types.CREATE_AUTHOR_SUCCESS,
+    author,
+  };
+}
+
+export function updateAuthorSuccess(author) {
+  return { type: types.UPDATE_AUTHOR_SUCCESS, author };
+}
+
+export function deleteAuthorOptimistic(author) {
+  return { type: types.DELETE_COURSE_OPTIMISTIC, author };
+}
+
 export function loadAuthors() {
   return function (dispatch) {
     dispatch(beginApiCall());
@@ -18,5 +33,29 @@ export function loadAuthors() {
         dispatch(apiCallError(error));
         throw error;
       });
+  };
+}
+
+export function saveAuthor(author) {
+  return function (dispatch) {
+    dispatch(beginApiCall());
+    return authorApi
+      .saveAuthor(author)
+      .then((savedAuthor) => {
+        author.id
+          ? dispatch(updateAuthorSuccess(savedAuthor))
+          : dispatch(createAuthorSuccess(savedAuthor));
+      })
+      .catch((error) => {
+        dispatch(apiCallError(error));
+        throw error;
+      });
+  };
+}
+
+export function deleteAuthor(author) {
+  return function (dispatch) {
+    dispatch(deleteAuthorOptimistic(author));
+    return authorApi.deleteAuthor(author.id);
   };
 }

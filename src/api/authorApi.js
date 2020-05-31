17d8266a1c82b0314eb50f9710b1a7f4b79b761b
Caio César Silva <caiocesarsilva08@gmail.com>
2020-05-31 09:36:44 -0300

    Stash changes

--- src/api/authorApi.js
+++ src/api/authorApi.js
@@ -3,9 +3,7 @@ const baseUrl = process.env.API_URL + "/authors/";
 
 export function getAuthors() {
   return fetch(baseUrl).then(handleResponse).catch(handleError);
-}
-
-export function saveAuthor(author) {
+}export function saveAuthor(author) {
   return fetch(baseUrl + (author.id || ""), {
     method: author.id ? "PUT" : "POST", // POST for create, PUT to update when id already exists.
     headers: { "content-type": "application/json" },
